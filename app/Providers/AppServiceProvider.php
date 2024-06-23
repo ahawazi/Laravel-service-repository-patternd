382c0d5af86c3217759c0d5b01c626996191a9a0
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-06-23 14:20:07 +0330

    feat (provider) : bind the service and repository in the app service repository

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Repositories\Interfaces\PostRepositoryInterface;
+use App\Repositories\PostRepository;
+use App\Services\Interfaces\PostServiceInterface;
+use App\Services\PostService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(PostServiceInterface::class, PostService::class);
+        $this->app->bind(PostRepositoryInterface::class, PostRepository::class);
     }
 
     /**
