c47be8deeea5f7319ac792169af658d249853cf4
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-06-23 13:13:59 +0330

    fix (interface) : change the some function in the PostServiceInterface

--- app/Services/Interfaces/PostServiceInterface.php
+++ app/Services/Interfaces/PostServiceInterface.php
@@ -4,9 +4,9 @@ namespace App\Services\Interfaces;
 
 interface PostServiceInterface
 {
-    public function getPosts();
+    public function getAllPosts();
     public function createPost($data);
     public function getPostById($id);
-    public function updatePost($id, $data);
+    public function updatePostById($data, $id);
     public function deletePostById($id);
 }
