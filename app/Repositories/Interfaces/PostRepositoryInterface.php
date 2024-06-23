35baf357e0f9595e35cbe98e692889e7a87d29e4
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-06-23 13:22:56 +0330

    fix (interface) : fix the some function in the PostRepositoryInterface

--- app/Repositories/Interfaces/PostRepositoryInterface.php
+++ app/Repositories/Interfaces/PostRepositoryInterface.php
@@ -5,8 +5,8 @@ namespace App\Repositories\Interfaces;
 interface PostRepositoryInterface
 {
     public function allPosts();
-    public function createPoost($data);
+    public function storePost($data);
     public function findPost($id);
     public function updatePost($data, $id);
-    public function deleetePost($id);
+    public function destroyPost($id);
 }
