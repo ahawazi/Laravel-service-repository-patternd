cd7995e5141b2604b539ec40c6b93810c34675ef
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-06-23 14:52:25 +0330

    feat (controller) : add the post controller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,63 +2,58 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
+use App\Services\Interfaces\PostServiceInterface;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    protected $postService;
+    public function __construct(PostServiceInterface $postService)
     {
-        //
+        $this->postService = $postService;
+    }
+    public function index(): Response
+    {
+        $posts = $this->postService->getAllPosts();
+        return Inertia::render('/Posts/Index', ['posts' => $posts]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function create(): Response
     {
-        //
+        return Inertia::render('/Posts/Create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function store(StorePostRequest $request)
     {
-        //
+        $this->postService->createPost($request->validated());
+        return redirect()->route('posts.index')->with('success', 'Post created successfully');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+    public function show($id): Response
     {
-        //
+        $post = $this->postService->getPostById($id);
+        return Inertia::render('Posts/Show', ['post' => $post]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+    public function edit($id): Response
     {
-        //
+        $post = $this->postService->getPostById($id);
+        return Inertia::render('/Posts/Edit', ['post' => $post]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function update(UpdatePostRequest $request, $id)
     {
-        //
+        $this->postService->updatePostById($request->validated(), $id);
+        return redirect()->route('posts.index')->with('success', 'Post updated successfully');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public function destroy($id)
     {
-        //
+        $this->postService->deletePostById($id);
+        return redirect()->route('posts.index')->with('success', 'Post deleted successfully');
     }
 }
